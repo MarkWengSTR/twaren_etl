1ac51faa7f63e8eb4fef0fef7ce57aaf5d1752be
Mark <bskin0330@gmail.com>
2021-04-08 09:51:38 +0800

    Refactor object prepare and add test structure

--- etl_twaren/es_api/object.py
+++ etl_twaren/es_api/object.py
@@ -1,45 +1,11 @@
 from elasticsearch import Elasticsearch
-import es_properties.object as es_ob_prop
-
-
-def prepare(object_ctx):
-    es_properties = object_ctx["es_properties"]
-
-    if "user" in es_properties:
-        object_ctx["es_object"] = Elasticsearch(
-            es_properties["end_point"], http_auth=(
-                es_properties["user"], es_properties["password"])
-        )
-    else:
-        object_ctx["es_object"] = Elasticsearch(es_properties["end_point"])
-
-    return object_ctx
-
-
-def data_es_prepare(ctx):
-    object_ctx = {
-        "es_object": None,
-        "es_properties": es_ob_prop.data_es
-    }
-
-    ctx["data_es_object"] = prepare(object_ctx)["es_object"]
-
-    return ctx
-
-
-def ml_es_prepare(ctx):
-    object_ctx = {
-        "es_object": None,
-        "es_properties": es_ob_prop.ml_es
-    }
-
-    ctx["analy_es_object"] = prepare(object_ctx)["es_object"]
-
-    return ctx
+import etl_twaren.es_properties.object as es_ob_prop
 
 
 def prepare_all(ctx):
-    data_es_prepare(ctx) and \
-        ml_es_prepare(ctx)
+    ctx["data_es_object"] = Elasticsearch(es_ob_prop.data_es["end_point"])
+    ctx["analy_es_object"] = Elasticsearch(
+        es_ob_prop.ml_es["end_point"], es_ob_prop.ml_es["user"], es_ob_prop.ml_es["password"]
+    )
 
     return ctx
