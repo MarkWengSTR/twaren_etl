a9377b02e89238d0ae948a951ef0933ab985c753
Mark <bskin0330@gmail.com>
2021-04-08 14:47:59 +0800

    Fix Elasticsearch argument error

--- etl_twaren/es_api/object.py
+++ etl_twaren/es_api/object.py
@@ -1,11 +1,12 @@
 from elasticsearch import Elasticsearch
-import etl_twaren.es_properties.object as es_ob_prop
+import es_properties.object as es_ob_prop
 
 
 def prepare_all(ctx):
     ctx["data_es_object"] = Elasticsearch(es_ob_prop.data_es["end_point"])
     ctx["analy_es_object"] = Elasticsearch(
-        es_ob_prop.ml_es["end_point"], es_ob_prop.ml_es["user"], es_ob_prop.ml_es["password"]
+        es_ob_prop.ml_es["end_point"],
+        http_auth=(es_ob_prop.ml_es["user"], es_ob_prop.ml_es["password"])
     )
 
     return ctx
