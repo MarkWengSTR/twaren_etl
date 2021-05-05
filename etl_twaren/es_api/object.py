176b1fc28f5b2bfd01dd225f79a2a549b756b723
Mark <bskin0330@gmail.com>
2021-05-05 11:34:08 +0800

    Remove http_auth

--- etl_twaren/es_api/object.py
+++ etl_twaren/es_api/object.py
@@ -4,9 +4,11 @@ import es_properties.object as es_ob_prop
 
 def prepare_all(ctx):
     ctx["data_es_object"] = Elasticsearch(es_ob_prop.data_es["end_point"])
-    ctx["analy_es_object"] = Elasticsearch(
-        es_ob_prop.ml_es["end_point"],
-        http_auth=(es_ob_prop.ml_es["user"], es_ob_prop.ml_es["password"])
-    )
+    ctx["analy_es_object"] = Elasticsearch(es_ob_prop.ml_es["end_point"])
 
     return ctx
+
+    # ctx["analy_es_object"] = Elasticsearch(
+    #     es_ob_prop.ml_es["end_point"],
+    #     http_auth=(es_ob_prop.ml_es["user"], es_ob_prop.ml_es["password"])
+    # )
